175c10cc6163c54edc92eedc3e1182e3c80c4b2d
SAM <sam@SAMs-MBP.mshome.net>
2019-02-21 17:08:14 +0100

    changed the db crendentials

--- btre/settings.py
+++ btre/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'btredb',
-        'USER': 'postgres',
-        'PASSWORD': '1q2w3e4r',
+        'NAME': 'btre_prod',
+        'USER': 'dbadmin',
+        'PASSWORD': 'abc123!',
         'HOST': 'localhost'
     }
 }
