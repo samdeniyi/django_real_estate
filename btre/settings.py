33d7ada5771b468a3c4ac9f5c26aadd6e0794300
SAM <sam@SAMs-MBP.mshome.net>
2019-02-21 16:21:37 +0100

    Added local setting

--- btre/settings.py
+++ btre/settings.py
@@ -150,3 +150,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'storesam86@gmail.com'
 EMAIL_HOST_PASSWORD = 'Oluwabunmi@1'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
