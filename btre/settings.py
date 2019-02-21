192ef97a1c08d0e5ed54665eedabe890a165d86d
SAM <sam@SAMs-MBP.mshome.net>
2019-02-21 17:38:09 +0100

    commented out datebase crendentials

--- btre/settings.py
+++ btre/settings.py
@@ -79,15 +79,15 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'btre_prod',
-        'USER': 'dbadmin',
-        'PASSWORD': 'abc123!',
-        'HOST': 'localhost'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'btre_prod',
+#         'USER': 'dbadmin',
+#         'PASSWORD': 'abc123!',
+#         'HOST': 'localhost'
+#     }
+# }
 
 
 # Password validation
@@ -147,3 +147,4 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
